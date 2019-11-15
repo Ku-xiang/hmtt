4cc978036648b2ad9b92e54eb175133b4493a73f
Ku-xiang <1114244884@qq.com>
2019-11-15 20:47:46 +0800

    黑马头条PC-主页模块-页面布局

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/' // 设置axios常态地址
 Vue.prototype.$axios = axios // 将axios共享给所有的实例使用
 Vue.config.productionTip = false
-Vue.use(Element)
+Vue.use(Element) // 注册整个的所有ElementUI组件 Vue.use 调用了element里面的一个方法=>install
 
 new Vue({
   router,
