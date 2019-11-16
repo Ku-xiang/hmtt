a5a499df8948f893e901f464ec52dc4b410aa527
Ku-xiang <1114244884@qq.com>
2019-11-16 20:43:10 +0800

    用户推出

--- src/components/layout-header.vue
+++ src/components/layout-header.vue
@@ -17,7 +17,7 @@
               <el-dropdown-menu solot="dropdown">
               <el-dropdown-item>个人信息</el-dropdown-item>
               <el-dropdown-item>git地址</el-dropdown-item>
-              <el-dropdown-item>退出</el-dropdown-item>
+              <el-dropdown-item @click.native="onLogout">退出</el-dropdown-item>
               </el-dropdown-menu>
           </el-dropdown>
       </el-col>
@@ -26,6 +26,27 @@
 
 <script>
 export default {
+  methods: {
+    onLogout () {
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        window.localStorage.removeItem('user-token')
+        this.$router.push('/login')
+        this.$message({
+          type: 'success',
+          message: '退出成功！'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
+  }
 
 }
 </script>
