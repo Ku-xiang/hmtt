d6e04662f761b316d542c0542cc0f5e7637cbd03
Ku-xiang <1114244884@qq.com>
2019-11-17 13:33:38 +0800

    文章列表-布局-表格

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -35,10 +35,43 @@
   <el-form-item>
     <el-button type="primary" @click="onSubmit">查询</el-button>
   </el-form-item>
-</el-form>
+  </el-form>
 </el-card>
-<!-- 筛选数据 -->
+<!-- /筛选数据 -->
 
+<!-- 文章列表 -->
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>共找到29860条符合条件的内容</span>
+    </div>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="封面"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="标题"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="发布状态">
+      </el-table-column>
+       <el-table-column
+        prop="address"
+        label="发布日期">
+      </el-table-column>
+       <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
+  </el-card>
+<!-- /文章列表 -->
   </div>
 </template>
 
@@ -52,7 +85,24 @@ export default {
         begin_pubdate: '',
         end_pubdate: ''
       },
-      rangeDate: ''
+      rangeDate: '',
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
   methods: {
