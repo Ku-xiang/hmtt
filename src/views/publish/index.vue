6695a42e3c7cc785819233b700e072d48ed22897
Ku-xiang <1114244884@qq.com>
2019-11-17 17:50:27 +0800

    发布文章-处理频道选择

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,11 +1,80 @@
 <template>
-  <div class="article">发布文章</div>
+  <div class="article">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>发布文章</span>
+      </div>
+      <el-form ref="form" :model="article" label-width="80px">
+  <el-form-item label="标题">
+    <el-input v-model="article.title"></el-input>
+  </el-form-item>
+   <el-form-item label="活动形式">
+    <el-input type="textarea" v-model="article.content"></el-input>
+  </el-form-item>
+  <el-form-item label="频道">
+   <el-select placeholder="请选择频道" v-model="article.channel_id">
+      <el-option
+      :label="channel.name"
+      :value="channel.id"
+      v-for="channel in channels"
+      :key='channel.id'
+      ></el-option>
+    </el-select>
+  </el-form-item>
+  <!-- <el-form-item label="封面"> -->
+    <!-- <el-radio-group v-model="form.resource">
+      <el-radio label="线上品牌商赞助"></el-radio>
+      <el-radio label="线下场地免费"></el-radio>
+    </el-radio-group> -->
+  <!-- </el-form-item> -->
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">发表</el-button>
+    <el-button>存入草稿</el-button>
+  </el-form-item>
+</el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
+
 export default {
+  name: 'publish',
+  data () {
+    return {
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {
+          type: 0, // 封面类型
+          images: []// 无图 空数组
+        },
+        channel_id: ''
+      },
+      channels: []
+    }
+  },
+  created () {
+    this.loadChannels()
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    loadChannels () {
+      this.$axios({
+        method: 'GET',
+        url: '/channels'
+      }).then(res => {
+        this.channels = res.data.data.channels
+      }).catch(err => {
+        console.log(err, '获取数据失败')
+      })
+    }
+  }
 
 }
+
 </script>
 
 <style>
