15165f9f58ec2f1c297f0b45c7f73ff1307352ce
Ku-xiang <1114244884@qq.com>
2019-11-19 21:32:00 +0800

    修改评论列表状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -11,17 +11,7 @@
         label="标题"
         width="180">
       </el-table-column>
-      <el-table-column
-        label="评论状态"
-        width="180">
-        <template slot-scope="scope">
-            <el-switch
-            v-model="scope.row.comment_status"
-            active-color="#13ce66"
-            inactive-color="#ff4949">
-            </el-switch>
-        </template>
-      </el-table-column>
+
       <el-table-column
         prop="total_comment_count"
         label="总评论数">
@@ -29,6 +19,18 @@
        <el-table-column
         prop="fans_comment_count"
         label="粉丝评论数">
+      </el-table-column>
+       <el-table-column
+        label="评论状态"
+        width="180">
+        <template slot-scope="scope">
+            <el-switch
+            v-model="scope.row.comment_status"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            @change="onStatusChange(scope.row)">
+            </el-switch>
+        </template>
       </el-table-column>
        <el-table-column
         label="操作">
@@ -56,7 +58,7 @@ export default {
   methods: {
     loadArticles () {
       this.$axios({
-        methods: 'GET',
+        method: 'GET',
         url: '/articles',
         params: {
           response_type: 'comment'
@@ -65,6 +67,26 @@ export default {
         this.articles = res.data.data.results
       }).catch(err => {
         console.log(err, '获取数据失败')
+      })
+    },
+    onStatusChange (article) {
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: article.id.toString()
+        },
+        data: {
+          // 开关组件双向绑定了 article.comment_status
+          allow_comment: article.comment_status
+        }
+      }).then(res => {
+        this.$message({
+          type: 'success',
+          message: `${article.comment_status ? '启用' : '失败'}成功`
+        })
+      }).catch(err => {
+        this.$message.error(err, '操作失败')
       })
     }
   }
